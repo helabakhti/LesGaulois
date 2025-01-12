ee71f141cc80ab4e4d4ca2c6862f11d9c2c86b38
helabakhti <bbkhela@gmail.com>
2025-01-12 21:18:55 +0100

    Debut class Romain

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,10 +1,23 @@
 package personnages;
 
 public class Gaulois {
+	
+	private String nom;
+	private int force;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
 
+	public String getNom() {
+		return nom;
+
+	}
+	
+	public void parler(String text) {
+		System.out.println("Le Gaulois " + nom + " dit :" + text);
 	}
 
 }
+
